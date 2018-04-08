d9447beadc8fdea17e6df304dc89abc7cdcf3e88
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-04-08 23:04:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ function takeANumber(katzDeliLine, newPerson){
 
 function nowServing(katzDeliLine){
   if (katzDeliLine>0){
-    firstInLine = katzDeliLine[0];
-    katzDeliLine.shift
+    return firstInLine = katzDeliLine[0];
+    katzDeliLine.shift();
+  }
+  else{
+    
   }
 }
