0d148229b6f87c43845bd0e0775adaddf983d80d
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-04-08 23:13:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, newPerson){
     katzDeliLine[katzDeliLine.length] = newPerson;
     var spotInLine = katzDeliLine.length+1;
-    return `Welcome ${newPerson}, you are number ${katzDeliLine} in line.`;
+    return `Welcome ${newPerson}, you are number ${spotInLine} in line.`;
 }
 
 function nowServing(katzDeliLine){
