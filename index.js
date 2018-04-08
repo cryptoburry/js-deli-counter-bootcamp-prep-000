7dd392a57d701591e1550b23d6c5c5febcf0a4b0
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-04-08 23:19:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ function currentLine(katzDeliLine){
   if(katzDeliLine.length>0){
   for(var i=0;i<katzDeliLine.length;i++){
     var spot = i+1;
-    lineString += `${spot}. ${katzDeliLine[i]}`;
+    lineString += `${spot}. ${katzDeliLine[i]} `;
   } return lineString;
   }
   else{
-    return 'The line is currently empty';
+    return 'The line is currently empty.';
   }
 }
